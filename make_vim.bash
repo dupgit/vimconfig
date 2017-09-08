e81d2d9284f87a8c5b14b13548e4ae61831f046f
dupgit <olivier.delhomme@free.fr>
2017-09-08 21:58:36 +0200

    Installs onedark vim theme.

--- make_vim.bash
+++ make_vim.bash
@@ -37,3 +37,9 @@ git clone --depth=1 https://github.com/scrooloose/syntastic.git
 mkdir -p ~/.vim/pack/default/start
 cd ~/.vim/pack/default/start
 git clone https://github.com/sheerun/vim-polyglot
+
+# Installs onedark theme
+git clone https://github.com/joshdick/onedark.vim.git /tmp/onedark.vim
+cp /tmp/onedark.vim/colors/onedark.vim ~/.vim/colors/
+cp /tmp/onedark.vim/autoload/onedark.vim ~/.vim/autoload/
+rm -fr /tmp/onedark.vim 
